bc648dd376f975162f9ac0aa3a6b08baac2ff090
Manideep <saimanideep2212@gmail.com>
2026-07-19 02:06:07 +0530

    Edit function added and CRUD operations done

--- src/pages/Companies.jsx
+++ src/pages/Companies.jsx
@@ -5,28 +5,56 @@ function Companies(){
     const [role,setRole] = useState("");
     const [status,setStatus] = useState("Applied");
     const [companies,setCompanies] = useState([]);
-function addCompany(){
-     if (company.trim() === "" || role.trim() === "") {
-        alert("Please fill all fields.");
-        return;
-    }
-    setCompanies(
-        [...companies,{
-            company,
-            role,
-            status
+    const [editIndex,setEditIndex] = useState(null);
+    const [isEditing,setIsEditing] = useState(false);
+
+    function addCompany(){
+        if (company.trim() === "" || role.trim() === "") {
+            alert("Please fill all fields.");
+            return;
         }
-    ]);
-    setCompany("");
-    setRole("");
-    setStatus("Applied");
-}
-function deleteCompany(indexToDel){
-    const updatedCompanies = companies.filter((item,index)=>
-        index!==indexToDel
-    );
-    setCompanies(updatedCompanies);
-}
+        setCompanies(
+            [...companies,{
+                company,
+                role,
+                status
+            }
+        ]);
+        setCompany("");
+        setRole("");
+        setStatus("Applied");
+    }
+    function updateCompany(){
+        const updatedCompanies = companies.map((item,index)=>{
+            if(index===editIndex){
+                return{
+                    company:company,
+                    role:role,
+                    status:status
+                }
+            };
+            return item;
+        });
+        setCompanies(updatedCompanies);
+        setCompany("");
+        setRole("");
+        setStatus("Applied");
+        setEditIndex(null);
+        setIsEditing(false);
+    }
+    function deleteCompany(indexToDel){
+        const updatedCompanies = companies.filter((item,index)=>
+            index!==indexToDel
+        );
+        setCompanies(updatedCompanies);
+    }
+    function editForm(item,index){
+        setEditIndex(index);
+        setIsEditing(true);
+        setCompany(item.company);
+        setRole(item.role);
+        setStatus(item.status);
+    }
     return(
         <>
             <h1>Companies page</h1>
@@ -48,17 +76,22 @@ function deleteCompany(indexToDel){
                     <option>Rejected</option>
                     <option>Offer</option>
                 </select>
-                <button onClick={addCompany}>Add Company</button>
+                {
+                    !isEditing? (<button onClick={addCompany}>Add Company</button>)
+                    :
+                    <button onClick={updateCompany}>Update Details</button>
+                }
+                
             </div>
+            <h2>Companies</h2>
             <div className="companyList">
-                 <h2>Companies</h2>
                     {companies.map((item,index)=>(
                         <div key={index} className="companyCard">
                             <h3>{item.company}</h3>
                             <p>{item.role}</p>
                             <p>{item.status}</p>
                             <div className = "button-container">
-                                <button>Edit</button>
+                                <button onClick={()=>editForm(item,index)}>Edit</button>
                                 <button onClick={()=> deleteCompany(index)}>Delete</button>
                             </div>
                         </div>
