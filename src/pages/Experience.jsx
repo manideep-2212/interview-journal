b056e1341be7deaea69739b933ff31372aecb195
Manideep <saimanideep2212@gmail.com>
2026-08-04 17:18:22 +0530

    Experience form updates

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -14,26 +14,57 @@ function Experience(){
             <h1>Experience page</h1>
             <div>
                 <label>Company</label>
-                <input value= {journalEntry.company} type="text" id="company" placeholder="Enter company name"/><br></br>
+                <input value= {journalEntry.company} 
+                onChange = {(e)=>setJournalEntry({
+                    ...journalEntry,
+                    company:e.target.value
+                })}
+                type="text" id="company" 
+                placeholder="Enter company name"/><br></br>
                 <label>Role Applied</label>
-                <input value= {journalEntry.role} type="text" id="role" placeholder="Enter the role applied"/><br></br>
+                <input value= {journalEntry.role} 
+                onChange={(e)=>setJournalEntry({
+                    ...journalEntry,
+                    role:e.target.value
+                })}
+                type="text" id="role" 
+                placeholder="Enter the role applied"/><br></br>
                 <label>Round Type</label>
-                <select value= {journalEntry.round}>
+                <select value= {journalEntry.round}
+                onChange={(e)=>setJournalEntry({
+                    ...journalEntry,
+                    round:e.target.value
+                })}>
                     <option>Online Assessment</option>
                     <option>Technical Round-1</option>
                     <option>Technical Round-2</option>
                     <option>GD</option>
                     <option>HR Round</option>
-                </select><br></br>
+                </select>
+                <br></br>
                 <label>Date</label>
-                <input value = {journalEntry.date} type="date" id="date" /><br></br>
+                <input value = {journalEntry.date} 
+                onChange={(e)=>setJournalEntry({
+                    ...journalEntry,
+                    date:e.target.value
+                })}
+                type="date" 
+                id="date" /><br></br>
                 <label>Result</label>
-                <select value={journalEntry.result}>
+                <select value={journalEntry.result}
+                onChange={(e)=>setJournalEntry({
+                    ...journalEntry,
+                    result:e.target.value
+                })}>
                     <option>Quailified</option>
                     <option>Disqualified</option>
                 </select><br></br>
                 <label>Experience and Learnings</label>
-                <textarea value={journalEntry.experience}></textarea><br></br>
+                <textarea value={journalEntry.experience}
+                onChange={(e)=>setJournalEntry({
+                    ...journalEntry,
+                    experience:e.target.value
+                })}></textarea><br></br>
                 <button>Save</button>
                 <button>Edit</button>
                 <button>Delete</button>
