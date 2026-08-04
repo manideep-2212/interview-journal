734f48820a4a60a5848e3234f53930e933ef20f0
Manideep <saimanideep2212@gmail.com>
2026-08-05 02:35:31 +0530

    Experince: Computed property Names

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,5 +1,4 @@
 import {useState} from "react";
-
 function Experience(){
     const [journalEntry,setJournalEntry] = useState({
         company:"",
@@ -9,32 +8,30 @@ function Experience(){
         result:"",
         experience:""
     });
+    function handleChange(e){
+    const variable = e.target.id;
+    setJournalEntry({
+        ...journalEntry,
+        [variable] : e.target.value
+    });
+}
     return(
         <>
             <h1>Experience page</h1>
             <div>
                 <label>Company</label>
                 <input value= {journalEntry.company} 
-                onChange = {(e)=>setJournalEntry({
-                    ...journalEntry,
-                    company:e.target.value
-                })}
+                onChange = {handleChange}
                 type="text" id="company" 
                 placeholder="Enter company name"/><br></br>
                 <label>Role Applied</label>
                 <input value= {journalEntry.role} 
-                onChange={(e)=>setJournalEntry({
-                    ...journalEntry,
-                    role:e.target.value
-                })}
+                onChange = {handleChange}
                 type="text" id="role" 
                 placeholder="Enter the role applied"/><br></br>
                 <label>Round Type</label>
-                <select value= {journalEntry.round}
-                onChange={(e)=>setJournalEntry({
-                    ...journalEntry,
-                    round:e.target.value
-                })}>
+                <select value= {journalEntry.round} id="round"
+                onChange = {handleChange}>
                     <option>Online Assessment</option>
                     <option>Technical Round-1</option>
                     <option>Technical Round-2</option>
@@ -43,34 +40,25 @@ function Experience(){
                 </select>
                 <br></br>
                 <label>Date</label>
-                <input value = {journalEntry.date} 
-                onChange={(e)=>setJournalEntry({
-                    ...journalEntry,
-                    date:e.target.value
-                })}
+                <input value = {journalEntry.date} id="date"
+                onChange = {handleChange}
                 type="date" 
                 id="date" /><br></br>
                 <label>Result</label>
                 <select value={journalEntry.result}
-                onChange={(e)=>setJournalEntry({
-                    ...journalEntry,
-                    result:e.target.value
-                })}>
+                onChange = {handleChange}>
                     <option>Quailified</option>
                     <option>Disqualified</option>
                 </select><br></br>
                 <label>Experience and Learnings</label>
-                <textarea value={journalEntry.experience}
-                onChange={(e)=>setJournalEntry({
-                    ...journalEntry,
-                    experience:e.target.value
-                })}></textarea><br></br>
+                <textarea id ="experience" value={journalEntry.experience}
+                onChange = {handleChange}></textarea><br></br>
                 <button>Save</button>
                 <button>Edit</button>
                 <button>Delete</button>
             </div> 
         </>
-        
+    
 
     );
 }
